d710b31fa3019e07d61bbdb575eaa66486b6faf0
Jovan <smiljko99@gmail.com>
2021-03-22 02:29:38 +0100

    games on sale added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React, { useEffect, useState } from "react";
-import Games from "./games";
+import Games from "./gamesOnSale/gamesOnSale";
+import Header from "./header/header";
 import "./App.css";
 
 function App() {
   const [games, setGames] = useState([]);
   useEffect(() => {
-    gameData();
+    gameOnSale();
   }, []);
 
-  const gameData = async () => {
+  const gameOnSale = async () => {
     const responce = await fetch("https://www.cheapshark.com/api/1.0/deals");
     const data = await responce.json();
     setGames(data);
-    // console.log(data);
   };
 
   return (
     <div className="App">
+      <Header />
       <div className="cont">
         {games.map(game => (
           <Games
