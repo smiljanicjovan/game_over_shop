50d99f85ca8a6c2b4d8a2d39ca937c81b5bdb6fa
Jovan Smiljanic <smiljko99@gmail.com>
2021-09-20 23:18:59 +0200

    env settings

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-  apiKey: REACT_APP_API_KEY,
-  authDomain: REACT_APP_AUTH_DOMAIN,
-  projectId: REACT_APP_PROJECT_ID,
-  storageBucket: REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_MESSANGING,
-  appId: REACT_APP_APP_ID,
+  apiKey: procces.env.REACT_APP_API_KEY,
+  authDomain: procces.env.REACT_APP_AUTH_DOMAIN,
+  projectId: procces.env.REACT_APP_PROJECT_ID,
+  storageBucket: procces.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: procces.env.REACT_APP_MESSANGING,
+  appId: procces.env.REACT_APP_APP_ID,
 });
 
 export const auth = app.auth();
