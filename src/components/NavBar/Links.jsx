5b61695bddc0ab0385f41daea81a76daee4a8483
Jovan Smiljanic <smiljko99@gmail.com>
2021-09-29 19:50:47 +0200

    navbar change

--- src/components/NavBar/Links.jsx
+++ src/components/NavBar/Links.jsx
@@ -7,24 +7,22 @@ import { Link } from "react-router-dom";
 export default function NavBarLinks() {
   const [showLinks, setShowLinks] = useState(false);
   return (
-    <>
-      <div className="navbar-links">
-        <div className="hamburger_btn" onClick={() => setShowLinks(!showLinks)}>
-          <Hamburger />
-        </div>
-
-        <ul id={showLinks ? "hidden" : ""}>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/deals">Best Deals</Link>
-          </li>
-          <li>
-            <Link to="/stores">Stores</Link>
-          </li>
-        </ul>
+    <div className="navbar-links">
+      <div className="hamburger_btn" onClick={() => setShowLinks(!showLinks)}>
+        <Hamburger />
       </div>
-    </>
+
+      <ul id={showLinks ? "hidden" : ""}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/deals">Best Deals</Link>
+        </li>
+        <li>
+          <Link to="/stores">Stores</Link>
+        </li>
+      </ul>
+    </div>
   );
 }
